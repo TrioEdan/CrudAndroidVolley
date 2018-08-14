04f31bf3a77db5559b9a2d9bd8acefa0024b5593
Muhammad Adam <emailnya.addam@gmail.com>
2018-08-14 22:07:13 +0700

    first commit

---
+++ app/src/main/java/com/example/muhammadadam/crudandroidvolley/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.muhammadadam.crudandroidvolley;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.muhammadadam.crudandroidvolley.Adapter.AdapterData;
+import com.example.muhammadadam.crudandroidvolley.Model.ModelData;
+import com.example.muhammadadam.crudandroidvolley.Util.AppController;
+import com.example.muhammadadam.crudandroidvolley.Util.ServerAPI;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mManager;
+    List<ModelData> mItems;
+    Button btnInsert, btnDelete;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycleViewTemp);
+        btnInsert = (Button) findViewById(R.id.btn_insert);
+        btnDelete = (Button) findViewById(R.id.btn_delete);
+        pd = new ProgressDialog(MainActivity.this);
+        mItems = new ArrayList<>();
+
+        loadJson();
+
+        mManager = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setLayoutManager(mManager);
+        mAdapter = new AdapterData(MainActivity.this,mItems);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+    private void loadJson(){
+        pd.setMessage("Mengambil Data");
+        pd.setCancelable(false);
+        pd.show();
+        JsonArrayRequest reqData = new JsonArrayRequest(Request.Method.POST, ServerAPI.URL_DATA, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        pd.cancel();
+                        Log.d("volley","response :" + response.toString());
+                        for (int i = 0 ; i < response.length(); i++){
+                            try {
+                                JSONObject data = response.getJSONObject(i);
+                                ModelData md = new ModelData();
+                                md.setNpm(data.getString("npm"));
+                                md.setNama(data.getString("nama"));
+                                md.setProdi(data.getString("prodi"));
+                                md.setKelas(data.getString("kelas"));
+                                mItems.add(md);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        mAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    pd.cancel();
+                    Log.d("volley","response :" + error.getMessage());
+                }
+                });
+        AppController.getInstance().addToRequestQueue(reqData);
+    }
+}
